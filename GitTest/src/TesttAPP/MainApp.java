fa9ca7346e31d205f5937936d500e84a0ae69b69
TinaWu <asd28018807@gmail.com>
2022-05-31 16:22:03 +0800

    3rd Version

--- GitTest/src/TesttAPP/MainApp.java
+++ GitTest/src/TesttAPP/MainApp.java
@@ -5,6 +5,8 @@ public class MainApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("第二個版本");
+		//我是組員我要修改
+		System.out.println("第三個版本");
 	}
 
 }
